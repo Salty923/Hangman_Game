ef26211511bf684c7ffcf76b9a180d778c90bf42
Scott Saltzman <salty923@gmail.com>
2017-11-27 21:20:14 -0600

    Fix word Array and add alphabet array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,10 @@
-var words=[ bells, blitzen, candy_canes, cards, chimney, comet, dancer, dasher, donner, eggnog, 
-elves, goodwill, merry, mistletoe, north_pole, lights, jolly, icicles, prancer, pine, 
-poinsettia, presents, reindeer, rudolph, santa, scrooge, sled, snowflakes, wrapping_paper, 
-wreath, yule, winter, toys, tinsel, stocking, star]
+var word =[ 'bells', 'blitzen', 'candy_canes', 'cards', 'chimney', 'comet', 
+'dancer', 'dasher', 'donner', 'eggnog', 'elves', 'goodwill', 'merry',
+'mistletoe', 'north_pole', 'lights', 'jolly', 'icicles', 'prancer',
+'pine', 'poinsettia', 'presents', 'reindeer', 'rudolph', 'santa', 
+'scrooge', 'sled', 'snowflakes', 'wrapping_paper', 'wreath', 'yule',
+ 'winter', 'toys', 'tinsel', 'stocking', 'star'];
+
+ var alpa = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+ 'o','p','q','r','s','t','u','v','w','x','y','z']
+
