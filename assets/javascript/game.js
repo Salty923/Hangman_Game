e1e9197b3a858b93503ce7bcd271265e1047a961
Scott Saltzman <salty923@gmail.com>
2017-11-27 20:58:43 -0600

    Set up front end style and heading. start JS word array

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+var words=[ bells, blitzen, candy_canes, cards, chimney, comet, dancer, dasher, donner, eggnog, 
+elves, goodwill, merry, mistletoe, north_pole, lights, jolly, icicles, prancer, pine, 
+poinsettia, presents, reindeer, rudolph, santa, scrooge, sled, snowflakes, wrapping_paper, 
+wreath, yule, winter, toys, tinsel, stocking, star]
