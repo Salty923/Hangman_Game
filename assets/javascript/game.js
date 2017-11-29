de2e7673ed7ebb1225bb2abcb388215836a4a9d4
Scott Saltzman <salty923@gmail.com>
2017-11-28 23:30:39 -0600

    Move External js to inside html in script tag, add random word generator and character generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,13 @@
+
+//Create an Array of random Christman words//
 var word =[ 'bells', 'blitzen', 'candy_canes', 'cards', 'chimney', 'comet', 
 'dancer', 'dasher', 'donner', 'eggnog', 'elves', 'goodwill', 'merry',
 'mistletoe', 'north_pole', 'lights', 'jolly', 'icicles', 'prancer',
 'pine', 'poinsettia', 'presents', 'reindeer', 'rudolph', 'santa', 
 'scrooge', 'sled', 'snowflakes', 'wrapping_paper', 'wreath', 'yule',
  'winter', 'toys', 'tinsel', 'stocking', 'star'];
+
+ // be able to generate random words from array//
 
  var alpa = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
  'o','p','q','r','s','t','u','v','w','x','y','z']
